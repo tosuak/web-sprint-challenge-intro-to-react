3c356527256895ea33a873c88759cf4985551180
Tafiqul <demianviotto@gmail.com>
2021-12-10 13:35:55 -0500

    finished displaying content working on styling

--- src/App.js
+++ src/App.js
@@ -7,24 +7,28 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([]);
-  const [charactersId, setCharactersId] = useState('1');
+  const [currentCharactersId, setCurrentCharactersId] = useState(null);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
   const openDetails = id => {
-    setCharactersId(id);
+    setCurrentCharactersId(id);
   }
 
   const closeDetails = id => {
-    setCharactersId(null);
+    setCurrentCharactersId(null);
   }
 
+  const CharacterName = props => (
+    <div className='character'>
+      <button onClick={() => openDetails(props.info)}>{props.info.name}</button>
+    </div>
+  )
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people`)
     .then(resp => {
-      console.log(resp);
+      console.log(resp.data);
       setCharacters(resp.data);
     })
     .catch(err => {
@@ -35,6 +39,14 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map(ch => {
+          return <CharacterName key={ch.name} info={ch} action={openDetails} />
+        })
+      }
+      {
+        currentCharactersId && <Character characterId={currentCharactersId} close={closeDetails} />
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,45 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+export default function Character(props) {
+    const {characterId, close} = props;
+    const [details, setDetails] = useState([]);
+    
+    useEffect(() => {
+        axios.get(characterId.url)
+        .then(resp => {
+            setDetails(resp.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })    
+    }, [characterId])
+
+    return (
+        <div className='container'>
+            <h2>Character Details:</h2>
+            {
+                details &&
+                <>
+                    <h3>Name: {details.name}</h3>
+                    <h4>Home World: {details.homeworld}</h4>
+                    <h4>Birth Year: {details.birth_year}</h4>
+                    <h4>Gender: {details.gender}</h4>
+                    <h3>
+                        Appearance: 
+                        <ul>
+                            <p>Height: {details.height}</p>
+                            <p>Mass: {details.mass}</p>
+                            <p>Skin Color: {details.skin_color}</p>
+                            <p>Hair Color: {details.hair_color}</p>
+                            <p>Eye Color: {details.eye_color}</p>
+                        </ul>
+                    </h3>
+                </>
+            }
+            <button onClick={close}>Close</button>
+        </div>
+    )
+}
