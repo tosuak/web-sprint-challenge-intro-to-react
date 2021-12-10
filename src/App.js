aa85757d9f2b13e086b3364701980cdf2ce7061e
Tafiqul <demianviotto@gmail.com>
2021-12-10 11:05:25 -0500

    I fetched the data from the mock api. Going to render it to display now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([]);
+  const [charactersId, setCharactersId] = useState('1');
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const openDetails = id => {
+    setCharactersId(id);
+  }
+
+  const closeDetails = id => {
+    setCharactersId(null);
+  }
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(resp => {
+      console.log(resp);
+      setCharacters(resp.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
 
   return (
     <div className="App">
